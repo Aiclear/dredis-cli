f4dc5b494a1fde70cb915312a92a24f90f2bd91b
blackz <446798939@qq.com>
2026-09-07 22:31:35 +0800

    add some command test

--- source/app.d
+++ source/app.d
@@ -18,4 +18,18 @@ void main() {
 
     auto notExist = redisClient.execCmd("GET notexist");
     writeln("GET notexist command result: ", notExist);
+
+    result = redisClient.execCmd("set name 余昕哲");
+    writeln("SET command result: ", result);
+
+    auto name = redisClient.execCmd("get name");
+    writeln("GET command result: ", name);
+
+    redisClient.execCmd("hset person name yuxinzhe");
+    auto personName = redisClient.execCmd("hget person name");
+    writeln("GET command person name: ", personName);
+
+    redisClient.execCmd("set age 20");
+    auto age = redisClient.execCmd("get age");
+    writeln("GET command result: ", age);
 }
